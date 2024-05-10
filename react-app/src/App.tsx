822b944413ad5b930591152521e1fec3c0ffcbbf
c4171208 <150667962+c4171208@users.noreply.github.com>
2024-05-10 11:28:59 +0300

    Uploading base files:ConfirmeOrder.css,ConfirmeOrder.tsx ,App.tsxb for the task-ConfirmeOrder

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Button } from '@mui/material';
 import BaseWizard from './Stepper/BaseWizard';
-
+import ConfirmeOrder from './ConfirmeOrder';
 function App() {
   const [showWizard, setShowWizard] = useState(false)
+  const newOrder = { _id: '123456' }; //מתוך קומפוננטת תשלום   מקבלים את הזמנה 
+  //                                ןשם  שולחים את את פרטי ההזמנה הנחוצים בלבד
+
 
   return (
     <div>
+      {/* זה אמור להיות כתוב מתוך קומפננטת תשלום-לא כאן */}
+      <ConfirmeOrder newOrder={newOrder} />
+
       {showWizard && (<BaseWizard />)}
 
       <Button
-          variant='contained'
-          size='large'
-          onClick={() => setShowWizard(!showWizard)}>
-          {showWizard ? 'hide wizard' : 'show wizard'}
-        </Button>
+        variant='contained'
+        size='large'
+        onClick={() => setShowWizard(!showWizard)}>
+        {showWizard ? 'hide wizard' : 'show wizard'}
+      </Button>
     </div>
   );
 }
