5d349c543e6d05e5dc946d72a9953e3cf9de8a52
GITruti9851 <rut9851@gmail.com>
2024-06-10 13:08:50 +0300

    menu

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ParentComponent from './Main_Menu/Menu';
-import Men_Page from './Main_Menu/Men_Page';
-import Boys_Page from './Main_Menu/Boys_Page';
-import Girls_Page from './Main_Menu/Girls_Page';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ParentComponent />} />
-        <Route path="/Men_Page" element={<Men_Page />} />
-        <Route path="/Boys_Page" element={<Boys_Page />} />
-        <Route path="/Girls_Page" element={<Girls_Page />} />
       </Routes>
     </Router>
   );
--- react-app/src/Main_Menu/Menu.tsx
+++ react-app/src/Main_Menu/Menu.tsx
@@ -2,15 +2,10 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Avatar from '@mui/material/Avatar';
 import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Tooltip from '@mui/material/Tooltip';
-import { BlurOnSharp } from '@mui/icons-material';
-import { Link } from 'react-router-dom'; 
-
 export default function ParentComponent() {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
   const open = Boolean(anchorEl);
@@ -73,31 +68,7 @@ export default function ParentComponent() {
           },
         }}
       >
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <BlurOnSharp fontSize="small" />
-          </ListItemIcon>
-          <Link to="/Men_Page" style={{ textDecoration: 'none', color: 'inherit' }}>
-            Men
-          </Link>
-        </MenuItem>
         <Divider />
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <BlurOnSharp fontSize="small" />
-          </ListItemIcon>
-          <Link to="/Girls_Page" style={{ textDecoration: 'none', color: 'inherit' }}>
-            Girls
-          </Link>
-        </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <BlurOnSharp fontSize="small" />
-          </ListItemIcon>
-          <Link to="/Boys_Page" style={{ textDecoration: 'none', color: 'inherit' }}>
-            Boys
-          </Link>
-        </MenuItem>
       </Menu>
     </React.Fragment>
   );
