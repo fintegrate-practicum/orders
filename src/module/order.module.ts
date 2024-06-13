d61245597dffa77fe5f6ebca8677ed7d2426c32a
c4171208 <150667962+c4171208@users.noreply.github.com>
2024-06-13 17:46:09 +0300

    temp

--- src/module/order.module.ts
+++ src/module/order.module.ts
@@ -1,16 +1,18 @@
-
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { OrderService } from '../service/order.service';
 import { OrderController } from '../controller/order.controller';
 import { Order, OrderSchema } from '../entities/order.entity'; 
 import { GeneralService } from '../service/general.service'
+// import {RabbitPublisherService} from '../rabbit-publisher/rabbit-publisher.service'
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
   ],
   controllers: [OrderController],
-  providers: [OrderService,GeneralService],
+  // providers: [OrderService,GeneralService,RabbitPublisherService],
+    providers: [OrderService,GeneralService],
+
 })
 export class OrderModule {}
