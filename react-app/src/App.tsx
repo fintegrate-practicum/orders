b5108b2c9a9645ca08893e29294b858a9f4d7622
c4171208 <150667962+c4171208@users.noreply.github.com>
2024-06-17 16:40:23 +0300

    fix

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,37 +1,18 @@
 import React from 'react';
-import { useState } from 'react';
 import './App.css';
-import { Button } from '@mui/material';
-import BaseWizard from './Stepper/BaseWizard';
+import SmallShoppingBag from './smallBag/SmallShoppingBag';
 import ConfirmeOrder from './ConfirmeOrder';
+
 function App() {
-  const [showWizard, setShowWizard] = useState(false)
   const newOrder = { _id: '123456' }; //מתוך קומפוננטת תשלום   מקבלים את הזמנה 
   //                                ןשם  שולחים את את פרטי ההזמנה הנחוצים בלבד
 
-
   return (
-    <div>
-      {/* זה אמור להיות כתוב מתוך קומפננטת תשלום-לא כאן */}
-      <ConfirmeOrder newOrder={newOrder} />
-
-      {showWizard && (<BaseWizard />)}
-
-      <Button
-        variant='contained'
-        size='large'
-        onClick={() => setShowWizard(!showWizard)}>
-        {showWizard ? 'hide wizard' : 'show wizard'}
-      </Button>
-    </div>
-
-import './App.css';
-import SmallShoppingBag from './smallBag/SmallShoppingBag';
-
-function App() {
-  return (
+    <>
+    {/* זה אמור להיות כתוב מתוך קומפננטת תשלום-לא כאן */}
+    <ConfirmeOrder newOrder={newOrder} />
     <SmallShoppingBag />
-
+    </>
   );
 }
 
