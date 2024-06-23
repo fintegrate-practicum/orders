cd9187d0fa7d3a6ec45e97aa6bf01c3c1a908b65
sa-ben <sarale1818@gmail.com>
2024-06-24 00:58:12 +0300

    finish task, all is run

---
+++ react-app/src/products/Product.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './product.css';
+
+interface ProductProps {
+    product: {
+        image: string;
+        name: string;
+        model: string;
+        description: string;
+        price: string;  // השתמשנו במחרוזת כאן
+        sizes: string;
+    };
+}
+
+const Product: React.FC<ProductProps> = ({ product }) => {
+    return (
+        <div className="single_product">
+            <img src={product.image} alt={product.name} />
+            <div className='product_details'>
+                <h3>{product.name}</h3>
+                <p>{product.price}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
