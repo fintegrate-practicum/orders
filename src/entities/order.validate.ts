8f305fe82df26a53934874f968e61902e45e464d
GITruti9851 <rut9851@gmail.com>
2024-06-25 15:46:40 +0300

    validation schema

--- src/entities/order.validate.ts
+++ src/entities/order.validate.ts
@@ -10,17 +10,40 @@ const objectIdValidator = (value, helpers) => {
 };
 
 export const orderValidationSchema = Joi.object({
-  user: Joi.string().optional(),
+  user: Joi.string().min(3).max(20).required().messages({  
+      'string.min': "name of user must be between at list 3 letters",
+      'string.max': "name of user must be maximum 20 letters",
+      'any.required': "user is required"
+  }),
   type: Joi.object({
-    city: Joi.string().required(),
-    street: Joi.string().required(),
-    numBuild: Joi.number().required()
+    city: Joi.string().min(3).max(20).required().messages({
+    'string.min':"name of city must to be at list 3 letters",
+    'string.max':"name of city must to be maximum 20 letters",
+    'any.required':"city is required"
+    }),
+    street: Joi.string().min(3).max(15).required().messages({
+      'string.min': "name of street must be at list 3 letters",
+      'string.max': "name of street must be maximum 15 letters",
+      'any.required': "street is required"
+    }),
+    numBuild: Joi.number().min(1).max(3).required().messages({
+      'string.min': "numBuild must be between at list 1 letters",
+      'string.max': "numBuild must be between maximum 3 letters",
+      'any.required': "numBuild is required"
+    })
   }).required(),
   status: Joi.string().valid(...Object.values(OrderStatus)).required(),
-  date: Joi.date().iso().required(),
-  businessCode: Joi.string().required(),
+  date: Joi.date().iso().min(1).required().messages({
+    'date.min': "min date must be positive",
+    'any.required': "date is required"
+  }),
+  businessCode: Joi.string().min(1).max(20).required().messages({
+    'string.min': "businessCode must be between at list 1 letters",
+    'string.max': "businessCode must be between maximum 3 letters",
+    'any.required': "businessCode is required"
+  }),
   id: Joi.string().custom(objectIdValidator, 'ObjectId validation').required(),
-  settingManeger: Joi.number().min(0).required().messages({
+  settingManeger: Joi.number().min(1).required().messages({
     'number.min': " must be positive",
     'any.required': "settingManeger is required"
   }),
