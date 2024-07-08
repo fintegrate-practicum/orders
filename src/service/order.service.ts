19773a6341760c18e7f39956f2cfdf77c80a0d7b
Hadasa Schechter <hadasa.schchter@vcita.com>
2024-07-08 21:26:33 +0300

    rename env var

--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -19,7 +19,7 @@ export class OrderService {
       const createdOrder = new this.orderModel(createOrderDto);
       var mailAdress: string;
       if(process.env.ENV=="DEVELOPMENT")
-          mailAdress= process.env.DEVTIMEMAIL
+          mailAdress= process.env.MAILGUN_EMAIL
       // else
       //   mailAdress=savedOrder.user.email
       
