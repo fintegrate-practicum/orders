8d440161d6940d1fca9eb7ef5d165cbd3cfbaa22
Hadasa Schechter <hadasa.schchter@vcita.com>
2024-07-14 09:37:49 +0300

    fix & make sure all unit tests can run

--- src/rabbit-publisher/rabbit-publisher.service.spec.ts
+++ src/rabbit-publisher/rabbit-publisher.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RabbitPublisherService } from './rabbit-publisher.service';
+import { ConfigService } from '@nestjs/config';
 
 describe('RabbitPublisherService', () => {
   let service: RabbitPublisherService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RabbitPublisherService],
+      providers: [RabbitPublisherService, ConfigService],
     }).compile();
 
     service = module.get<RabbitPublisherService>(RabbitPublisherService);
