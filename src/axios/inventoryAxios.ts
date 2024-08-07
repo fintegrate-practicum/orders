2bffc7d6b709d21bf7d00a03553929d64b3b3fd1
yael-hamburger <yaelha640@gmail.com>
2024-08-07 13:08:51 +0300

    Update inventoryAxios.ts

--- src/axios/inventoryAxios.ts
+++ src/axios/inventoryAxios.ts
@@ -1,10 +1,5 @@
 import axios from 'axios';
-import * as dotenv from 'dotenv';
-import { join } from 'path';
-// Specify the path to your .env file relative to the current file
-const envPath = join(__dirname, '..', '..', 'infrastructure', '.env');
-// Load environment variables from the specified .env file
-dotenv.config({ path: envPath });
+
 const inventoryServiceURL = process.env.VITE_DOCKER_INVENTORY_SERVER_URL;
 if (!inventoryServiceURL) {
   throw new Error(
