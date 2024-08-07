45158a2a8b2f7ae6d0d6b364171fec1d2aca20a2
yael-hamburger <yaelha640@gmail.com>
2024-08-07 14:00:03 +0300

    Update cart.module.ts
    לא צריך,
    בהתחלה לא השתמשנו ברידקס

--- src/module/cart.module.ts
+++ src/module/cart.module.ts
@@ -3,12 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { CartController } from 'src/controller/cart.controller';
 import { Cart, CartSchema } from 'src/entities/cart.entity';
 import { CartService } from 'src/service/cart.service';
-import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Cart.name, schema: CartSchema }]),
-    HttpModule,
   ],
   controllers: [CartController],
   providers: [CartService],
