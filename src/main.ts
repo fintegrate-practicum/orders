c2df74d468a6ea53cd3cd7ca83a0a0aead9db5b3
SheviEngelsberg <s4161823@gmail.com>
2024-08-12 12:52:17 +0300

    fix logger in main

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PapertrailLogger } from './logger';
-import { Logger } from '@nestjs/common';
 
 
 async function bootstrap() {
-  const logger = new Logger("main");
   const app = await NestFactory.create(AppModule);
   const papertrailLogger = app.get(PapertrailLogger);
   app.useLogger(papertrailLogger);  
   // לבדוק שזה עובד!!-הוספת הגדרת !!CORS לכל הדומיינים (למטרות פיתוח בלבד)
   app.enableCors();
   await app.listen(4000);
-  logger.log('Server is running on http://localhost:4000');
+  papertrailLogger.log('Server is running on http://localhost:4000');
 }
 bootstrap();
